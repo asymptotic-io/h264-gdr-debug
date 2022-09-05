9d014ab623915cb5db14704cac647fbbc29e9e42
Sanchayan Maity <sanchayan@sanchayanmaity.net>
2022-09-05 20:12:57 +0530

    Fix black frame generation as expected by v4l2h264dec

--- gen-black-frame.sh
+++ gen-black-frame.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-gst-launch-1.0 videotestsrc pattern=black num-buffers=1 ! video/x-raw,format=I420,width=1920,height=1440 ! x264enc ! h264parse ! video/x-h264,stream-format=avc ! filesink location=black.h264 -v
+gst-launch-1.0 videotestsrc pattern=black num-buffers=1 ! video/x-raw,format=I420,width=1920,height=1440 ! x264enc ! h264parse ! video/x-h264,stream-format=byte-stream,alignment=au ! filesink location=black.h264 -v
